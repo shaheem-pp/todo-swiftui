2b0488c7403021cbc2ac8e842b5441168d7ffc4f
shaheem-pp <shanofficial2000@gmail.com>
2024-03-20 13:17:36 +0530

    Animation

--- ToDoList/View/NoItemsView.swift
+++ ToDoList/View/NoItemsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NoItemsView: View {
     
     @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
     
     var body: some View {
         ScrollView{
@@ -25,16 +26,16 @@ struct NoItemsView: View {
                         .font(.headline)
                         .frame(height: 55)
                         .frame(maxWidth: .infinity)
-                        .background(animate ? Color.red : Color.accentColor)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
                         .cornerRadius(10)
                         
                 })
                 .padding(.horizontal, animate ? 30 : 50)
                 .shadow(
-                    color: animate ? Color.red : Color.accentColor,
-                    radius: 10,
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
                     x: 0.0,
-                    y: 0.0
+                    y: animate ? 50.0 : 30.0
                 )
                 .scaleEffect(animate ? 1.1 : 1.0)
                 .offset(y: animate ? -7 : 0)
